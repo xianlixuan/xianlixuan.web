c53ee1efe0567d007a68d0cbad6b8d670f3eed66
Dogsausage <wanginfotechio@gmail.com>
2026-06-10 23:13:37 +0700

    Initial commit

---
+++ src/i18n/translation.ts
@@ -0,0 +1,36 @@
+import { getResolvedSiteLang } from "@utils/language";
+import { en } from "./languages/en";
+import { zh } from "./languages/zh";
+import { ja } from "./languages/ja";
+import type I18nKey from "./i18nKey";
+
+
+export type Translation = {
+    [K in I18nKey]: string;
+};
+
+const defaultTranslation = en;
+
+const map: { [key: string]: Translation } = {
+    en: en,
+    en_us: en,
+    en_gb: en,
+    en_au: en,
+    zh: zh,
+    zh_cn: zh,
+    zh_hk: zh,
+    zh_mo: zh,
+    zh_tw: zh,
+    zh_sg: zh,
+    ja: ja,
+    ja_jp: ja,
+};
+
+export function getTranslation(lang: string): Translation {
+    return map[lang.toLowerCase()] || defaultTranslation;
+}
+
+export function i18n(key: I18nKey): string {
+    const lang = getResolvedSiteLang();
+    return getTranslation(lang)[key];
+}
