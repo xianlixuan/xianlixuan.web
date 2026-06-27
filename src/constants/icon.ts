54f275034da753dc7379405170a645af407ddaf4
Dogsausage <wanginfotechio@gmail.com>
2026-06-28 01:45:14 +0700

    ัั

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -3,7 +3,7 @@ import type { Favicon } from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
     {
-        src: "/favicon/moon-pixel-20260628.png",
-        sizes: "1280x1280",
+        src: "/favicon/moon-gold-20260628.png",
+        sizes: "1024x1024",
     },
 ];
